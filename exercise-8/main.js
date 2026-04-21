8687aea4795e7dd337e3ed2a4f2f799ef7bf2028
fatima hassan <fatimahasan774@gmail.com>
2026-04-21 13:20:26 +0300

    exercise -8

--- exercise-8/main.js
+++ exercise-8/main.js
@@ -1,1 +1,21 @@
-console.log('hello world');
+let colors =['red', 'white', 'black'];
+console.log(colors);
+
+console.log('push')
+colors.push('blue');
+console.log(colors)
+
+console.log('pop')
+colors.pop()
+console.log(colors)
+
+
+console.log('unshift')
+colors.unshift('cyan')
+console.log(colors)
+
+console.log('shift')
+colors.shift()
+console.log(colors)
+
+
