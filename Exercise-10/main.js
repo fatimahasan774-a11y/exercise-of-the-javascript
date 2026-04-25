7d09af8abec94c81f9b889ec165a47ca78b3f193
fatima hassan <fatimahasan774@gmail.com>
2026-04-25 14:52:04 +0300

    upload exercise then

---
+++ Exercise-10/main.js
@@ -0,0 +1,14 @@
+let library=[
+    {
+        title: "Atomic-habits",
+        author: "james clear",
+        year: "2001"
+    },
+    {
+        title: "all about love",
+        author: "bell books",
+        year: "2021"
+    }
+]
+console.log(library[0].title)
+console.log(library[1].author)
