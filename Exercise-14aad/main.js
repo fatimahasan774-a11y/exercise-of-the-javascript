e12fdbce276cda7a8d063c3da66ae4fe4e99227a
fatima hassan <fatimahasan774@gmail.com>
2026-05-13 08:02:29 +0300

    upload exercise 14

---
+++ Exercise-14aad/main.js
@@ -0,0 +1,23 @@
+// let i=0;
+// do{
+//     console.log('number' +i)
+//     i++
+
+// }while( i < 5 )
+
+
+
+// let password;
+// do{
+// password = prompt("Enter your password")
+
+// } while(password !== "secret")
+//     console.log('password')
+
+
+let number;
+do{
+    number =prompt('enter your number 10')
+
+}while(number !== '10')
+    console.log('welcome number: 10')
