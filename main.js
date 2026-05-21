0878a8b793380a45d3f0fcb896cf3f36c5f2b0d6
fatima hassan <fatimahasan774@gmail.com>
2026-05-21 11:41:01 +0300

    upload exercise-17

---
+++ main.js
@@ -0,0 +1,13 @@
+let temperature =0;
+if(temperature ){
+
+}else if( temperature > 0){
+    console.log('very cold')
+
+}else if(temperature >0-15 ){
+console.log('cold')
+}else if(temperature > 15-25 ){
+    console.log('warm')
+}else if(temperature > 25){
+    console.log('hot')
+}
