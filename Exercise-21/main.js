cb769aeb0e5779b07cbc63f711deeff8e7571f14
fatima hassan <fatimahasan774@gmail.com>
2026-05-23 10:43:40 +0300

    upload exercise=21 teacher

---
+++ Exercise-21/main.js
@@ -0,0 +1,18 @@
+// let numbers=[10,5,80,44,22];
+// let evenNumber=numbers.filter((num)=> num %2===0);
+// console.log(evenNumber)
+
+
+// let score=[90,20,80,100,30,50];
+// let pass=score.filter((score)=> score >=  60 );
+// console.log(score)
+
+
+
+
+
+
+let numbers=[1,2,3,4,5];
+
+let add=numbers.reduce((totel,num)=> totel *num);
+console.log(add)
