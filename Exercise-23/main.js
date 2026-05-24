fe6f463ebee4aecaa2aa6659a7bceca72727d0a5
fatima hassan <fatimahasan774@gmail.com>
2026-05-24 10:17:37 +0300

    upload exercise 24 check it

--- Exercise-23/main.js
+++ Exercise-23/main.js
@@ -2,3 +2,25 @@ let car={make: 'Tayoto', model:'corolla', year:'2026'};
 let{make,model}=car
 console.log(make)
 console.log(model)
+
+
+
+// function greet (name='fatima'){
+//     console.log(`hello ${name}`)
+// }
+
+// greet('hajara')
+
+// function calculateArea(width, height=width){
+//     const area= width*height
+//     console.log(area)
+    
+// }
+// calculateArea(5);
+
+
+// function calculate(weight,height){
+//     let result =weight/height**2;
+//     console.log(result)
+// }
+// calculate(70,1.75)
---
+++ Exercise-24/main.js
@@ -0,0 +1,5 @@
+function calculateArea(width, height=width){
+    let result =width *height
+    console.log(result)
+}
+calculateArea(20)
