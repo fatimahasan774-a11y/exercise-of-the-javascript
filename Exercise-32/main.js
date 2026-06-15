41ba4f276c202597c97e8033ecd0f8ece7ad793c
fatima hassan <fatimahasan774@gmail.com>
2026-06-15 08:34:48 +0300

    upload this is exercise-33

--- Exercise-32/main.js
+++ Exercise-32/main.js
@@ -1,5 +1,18 @@
-const pragraph = document.querySelector("p")
+const pragraph = document.querySelector(".text")
 console.log(pragraph)
 
 const para =document.querySelectorAll("p")
 console.log(para)
+
+function changecontent(){
+    header.textContent =  "hellow world";
+}
+
+changecontent()
+
+
+
+function getElement(){
+    text.innerHTML = "welcome fulll websiete";
+}
+getElement()
---
+++ Exercise.33/main.js
@@ -0,0 +1,17 @@
+const para = document.getElementsByClassName('header');
+console.log(para)
+
+const text = document.getElementById('h2')
+console.log(text)
+
+
+
+
+
+function changeElement(){
+    para.textContent = "welcome to the dom"
+}
+
+function changeContent(){
+    text.innerHTML= "fatima hasam mahamoud"
+}
